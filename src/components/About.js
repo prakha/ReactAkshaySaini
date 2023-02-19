827280040d7e9e7712984b4d43e4043c9e71c938
Prakhar Dewan <prakhardewan765@gmail.com>
2023-02-19 17:29:12 +0530

    added Profile.js file

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,10 @@
-
-
-
-
+import { Outlet } from "react-router-dom";
 const About = () => {
   return (
     <div>
       <h1>About us page</h1>
       <h1>This is react js about us page</h1>
-      
+      <Outlet/>
     </div>
   );
 };
